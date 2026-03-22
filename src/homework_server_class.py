43ab8b611c2c4649cbdb37c30635fc71d22dc419
Mariya_Suchkova <mashakraxmal@gmail.com>
2026-03-22 18:12:16 +0300

    add feature class HWServer (do_GET, do_POST)

--- src/homework_server_class.py
+++ src/homework_server_class.py
@@ -32,23 +32,24 @@ class HWServer(BaseHTTPRequestHandler):
         print(body)
         self.send_response(200)
         self.send_header("Content-type", "text/html")
+        self.end_headers()
         write = "POST запрос обработан"
         self.wfile.write(bytes(write.encode('utf-8')))
 
 
 if __name__ == '__main__':
 
-    # webServer = HTTPServer((hostName, serverPort), HWServer)
-    # print("Server started http://%s:%s" % (hostName, serverPort))
-    #
-    # try:
-    #     webServer.serve_forever()
-    # except KeyboardInterrupt:
-    #     pass
-    #
-    #     # Остановка веб-сервера
-    # webServer.server_close()
-    # print("Server stopped.")
+    webServer = HTTPServer((hostName, serverPort), HWServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+
+    webServer.server_close()
+    print("Server stopped.")
 
     webServer= HTTPServer((hostName, serverPort), HWServer)
     print("Server is running on port 8080")
